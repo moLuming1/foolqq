a0e060378101a7420302683361a38be4d327a1c6
史亚风 <1491087363@qq.com>
2017-10-13 14:11:50 +0800

    使之支持发送图片

--- src/main/java/foolqq/BaseQQWindowContext.java
+++ src/main/java/foolqq/BaseQQWindowContext.java
@@ -1,7 +1,7 @@
 package foolqq;
 
-
 import java.awt.AWTException;
+import java.awt.Image;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
@@ -9,12 +9,11 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map; 
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.imageio.ImageIO;
 import org.jnativehook.GlobalScreen;
 import org.jnativehook.NativeHookException;
@@ -27,16 +26,16 @@ import foolqq.tool.QQWindowTool;
 
 public abstract class BaseQQWindowContext {
 
-	private Map<String, QQWindow> map = new  HashMap<String, QQWindow>();
+	private Map<String, QQWindow> map = new HashMap<String, QQWindow>();
 
 	private Robot robot;
 
 	private int interval = 200;
 
-	private int checkInterval=5;
-	
+	private int checkInterval = 5;
+
 	private BufferedImage pImage;
-	
+
 	public int getCheckInterval() {
 		return checkInterval;
 	}
@@ -51,33 +50,35 @@ public abstract class BaseQQWindowContext {
 
 	public BaseQQWindowContext(File point) throws AWTException, IOException, NativeHookException {
 		robot = new Robot();
-		pImage =ImageIO.read(point);
-		WindowHandleTask wintask = new WindowHandleTask(this,map,robot);
-  		Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(wintask, checkInterval, checkInterval, TimeUnit.SECONDS);
-  		Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
-    	logger.setLevel(Level.OFF); 
-    	logger.setUseParentHandlers(false); 
-  		GlobalScreen.registerNativeHook(); 
-        GlobalScreen.addNativeKeyListener(new  GlobalKeyListener());
+		pImage = ImageIO.read(point);
+		WindowHandleTask wintask = new WindowHandleTask(this, map, robot);
+		Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(wintask, checkInterval, checkInterval,
+				TimeUnit.SECONDS);
+		Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+		logger.setLevel(Level.OFF);
+		logger.setUseParentHandlers(false);
+		GlobalScreen.registerNativeHook();
+		GlobalScreen.addNativeKeyListener(new GlobalKeyListener());
 	}
 
-	public abstract void onMessage(String name,QQMsg msg);
-	
+	public abstract void onMessage(String name, QQMsg msg);
+
 	public synchronized void clearQQMsg() {
 		robot.keyPress(KeyEvent.VK_F10);
 		robot.keyRelease(KeyEvent.VK_F10);
 	}
-	
-	public synchronized String readQQMsg(String name )  { 
+
+	public synchronized String readQQMsg(String name) {
 		int x = map.get(name).getX();
 		int y = map.get(name).getY();
-		if(x==0||y==0) return null;
-		BufferedImage image = QQWindowTool.getScreen(robot); 
-		int height = image.getHeight();   
-		for (int i = x - 100; i < x+200; ++i) {
-			for (int j = y + 100; j <  height-100; ++j) {
-				if (QQWindowTool.isEqual(i, j, image, pImage)) { 
-					robot.mouseMove(i+150, j - 100);  
+		if (x == 0 || y == 0)
+			return null;
+		BufferedImage image = QQWindowTool.getScreen(robot);
+		int height = image.getHeight();
+		for (int i = x - 100; i < x + 200; ++i) {
+			for (int j = y + 100; j < height - 100; ++j) {
+				if (QQWindowTool.isEqual(i, j, image, pImage)) {
+					robot.mouseMove(i + 150, j - 100);
 					robot.delay(interval);
 					robot.mousePress(InputEvent.BUTTON1_MASK);
 					robot.mouseRelease(InputEvent.BUTTON1_MASK);
@@ -85,11 +86,11 @@ public abstract class BaseQQWindowContext {
 					robot.keyPress(KeyEvent.VK_CONTROL);
 					robot.keyPress(KeyEvent.VK_A);
 					robot.keyRelease(KeyEvent.VK_A);
-					robot.delay(interval); 
+					robot.delay(interval);
 					robot.keyPress(KeyEvent.VK_C);
 					robot.keyRelease(KeyEvent.VK_C);
 					robot.keyRelease(KeyEvent.VK_CONTROL);
-					robot.delay(interval);  
+					robot.delay(interval);
 					return ClipboardTool.getSystemClipboard();
 				}
 			}
@@ -97,21 +98,36 @@ public abstract class BaseQQWindowContext {
 		return null;
 	}
 
-	public synchronized void writeQQMsg(String name, String msg) {
+	public synchronized void writeQQMsg(String name, Object msg) {
 		int x = map.get(name).getX();
 		int y = map.get(name).getY();
-		if(x==0||y==0) return; 
-		BufferedImage image = QQWindowTool.getScreen(robot); 
+		if (x == 0 || y == 0)
+			return;
+		BufferedImage image = QQWindowTool.getScreen(robot);
 		int height = image.getHeight();
-		for (int i = x - 100; i < x+200; ++i) {
-			for (int j = y + 100; j < height-100; ++j) {
-				if (QQWindowTool.isEqual(i, j, image,pImage)) {	 
-					robot.mouseMove(i+150, j + 50);
+		for (int i = x - 100; i < x + 200; ++i) {
+			for (int j = y + 100; j < height - 100; ++j) {
+				if (QQWindowTool.isEqual(i, j, image, pImage)) {
+					robot.mouseMove(i + 150, j + 50);
 					robot.delay(interval);
 					robot.mousePress(InputEvent.BUTTON1_MASK);
 					robot.mouseRelease(InputEvent.BUTTON1_MASK);
 					robot.delay(interval);
-					ClipboardTool.setSysClipboardText(msg);
+					if (msg instanceof String) {
+						ClipboardTool.setSysClipboardText((String) msg);
+					} else if (msg instanceof File) {
+						Image imgObj = null;
+						try {
+							imgObj = ImageIO.read((File) msg);
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+						ClipboardTool.setClipboardImage(imgObj);
+
+					} else if (msg instanceof Image) {
+						ClipboardTool.setClipboardImage((Image) msg);
+					}
+
 					robot.keyPress(KeyEvent.VK_CONTROL);
 					robot.keyPress(KeyEvent.VK_A);
 					robot.keyRelease(KeyEvent.VK_A);
@@ -127,4 +143,5 @@ public abstract class BaseQQWindowContext {
 			}
 		}
 	}
+
 }
